4461abe74921fdf91fecf8d03b44ffca8a122d80
ggumiblv <gulnazhakimova70@gmail.com>
2024-11-26 13:08:07 +0300

    added structure and styles

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const { override, addWebpackAlias } = require('customize-cra');
+
+module.exports = override(
+  addWebpackAlias({
+    '@styles': path.resolve(__dirname, 'src/shared/styles'),
+  })
+);
---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      hello 
+    </div>
+  );
+}
+
+export default App;
