4b0458f2f78c1af294923f190da7697d67bfe8fb
ggumiblv <gulnazhakimova70@gmail.com>
2024-12-01 12:09:57 +0300

    fix fetch

--- src/shared/components/departments-block/index.tsx
+++ src/shared/components/departments-block/index.tsx
@@ -4,11 +4,11 @@ import DepartmentCard from "../../ui/department-card";
 import ArrowRightIcon from "../../assets/icons/arrow-right-icon";
 import Button from "../../ui/button";
 import FeedbackPopup from "../feedback-popup";
-// import SuccessPopup from "../success-popup";
+import SuccessPopup from "../success-popup"; // Убедитесь, что этот компонент существует
 
 const DepartmentsBlock: React.FC = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  // const [isSuccessPopupOpen, setIsSuccessPopupOpen] = useState(false);
+  const [isSuccessPopupOpen, setIsSuccessPopupOpen] = useState(false); // Состояние для попапа успеха
 
   const handleOpenPopup = () => {
     setIsPopupOpen(true);
@@ -18,9 +18,9 @@ const DepartmentsBlock: React.FC = () => {
     setIsPopupOpen(false);
   };
 
-  // const handleCloseSuccessPopup = () => {
-  //   setIsSuccessPopupOpen(false);
-  // };
+  const handleCloseSuccessPopup = () => {
+    setIsSuccessPopupOpen(false);
+  };
 
   return (
     <div className={styles.widget}>
@@ -69,13 +69,15 @@ const DepartmentsBlock: React.FC = () => {
           isOpen={isPopupOpen}
           onClose={handleClosePopup}
           onSubmit={() => {
-            // setIsSuccessPopupOpen(true);
             setIsPopupOpen(false);
+            setIsSuccessPopupOpen(true); // Открываем попап успеха
           }}
         />
       )}
 
-      {/* {isSuccessPopupOpen && <SuccessPopup onClose={handleCloseSuccessPopup} />} */}
+      {isSuccessPopupOpen && (
+        <SuccessPopup onClose={handleCloseSuccessPopup} />
+      )}
     </div>
   );
 };
